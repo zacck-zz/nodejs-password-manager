69fbdbd0ffee9b849259697a84e9562802e8f721
zacck <coderv63@gmail.com>
2017-03-23 09:42:56 +0200

    work on persisting and reading data

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 console.log('starting password manager');
+
+var storage = require('node-persist');
+//initialize storage
+storage.initSync();
+
+
+//save
+//key , value
+//storage.setItemSync('name', 'Zacck');
+
+//get from storage
+var name = storage.getItemSync('name');
+
+console.log('saved name is', name);
